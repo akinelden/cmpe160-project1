216f396b56cb90c69345034c256270822b8d3dc8
Akin Elden <akin.elden@boun.edu.tr>
2019-07-15 17:23:34 +0300

    DialogManager class is finalized

--- src/utilities/DialogManager.java
+++ src/utilities/DialogManager.java
@@ -13,6 +13,13 @@ public class DialogManager{
     private static final String[] messageTitles = {"Error", "Info", "Warning", "Question"};
     
 
+    /**
+     * Shows a message to the user
+     * 
+     * @param message Message to be showed
+     * @param messageType Type of the message
+     * @param parent Parent component of the window
+     */
     public static void showMessage(String message, int messageType, Component parent){
         if(messageType<0 || messageType > 3){
             messageType = 0;
@@ -20,14 +27,34 @@ public class DialogManager{
         JOptionPane.showMessageDialog(parent, message, messageTitles[messageType], messageType);
     }
 
+    /**
+     * Shows an error message to the user.
+     * @param message Message to be showed
+     * @param parent Parent component of the window
+     */
     public static void informException(String message, Component parent){
         showMessage(message, ERROR_MSG, parent);
     }
 
-    public static boolean askForAction(Exception e, String message, Component parent){
+    /**
+     * Creates a dialog window with YES/NO options and shows the message to the user.
+     * It is used to confirm an action by the user.
+     * @param message Message to be showed
+     * @param parent Parent component of the window
+     * @return Returns <code>true</code> if user presses YES, <code>false</code> otherwise.
+     */
+    public static boolean askForAction(String message, Component parent){
         return JOptionPane.showConfirmDialog(parent, message, "Warning", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION ? true : false;
     }
 
+    /**
+     * Creates a dialog window with YES/NO/CANCEL options and shows the message to the user.
+     * @param message Message to be showed
+     * @param parent Parent component of the window
+     * @return Returns <code>0</code> if user presses YES, 
+     * <code>1</code> if user presses NO, 
+     * <code>2</code> if user presses CANCEL.
+     */
     public static int showConfirmDialog(String message, Component parent){
         return JOptionPane.showConfirmDialog(parent, message, "Warning", JOptionPane.YES_NO_CANCEL_OPTION);
     }
